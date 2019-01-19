954241d648bdaeb95020f2a2e3aa24a1b38f6a3e
Avi <avinash-oza@users.noreply.github.com>
2019-01-19 15:14:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-        name='gdb-rest-service',
+        name='db-rest-service',
         version='0.1b1',
         packages=['db_rest_service',],
         license='TBD',
