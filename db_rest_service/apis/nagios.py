6511e7b43bca6217d5fe7f525eed81072c0385a8
Avi Oza <avioza@gmail.com>
2019-01-19 14:00:42 -0500

    parse datetimes on upload to the database

--- db_rest_service/apis/nagios.py
+++ db_rest_service/apis/nagios.py
@@ -1,6 +1,8 @@
+import datetime
 from flask import request
 from flask_restplus import Namespace, Resource, fields, reqparse
 from pymysql.cursors import DictCursor
+from dateutil.parser import parse
 
 from db_rest_service import app
 from db_rest_service.db_config import mysql_nagios
@@ -16,16 +18,31 @@ NagiosAlertListModel = api.model('NagiosAlertListModel', {
     'alerts': fields.List(fields.Nested(NagiosAlertModel))
 })
 
-NagiosInsertAlertModel = api.model('NagiosInsertAlertModel',
-                                   {'hostname': fields.String(),
-                                    'service_name': fields.String(),
-                                    'notification_type': fields.String(),
-                                    'message_text': fields.String()
-                                   })
-
 parser = reqparse.RequestParser()
 parser.add_argument('type', type=str, choices=['UNSENT', 'ALL'], default='UNSENT', help='types of alerts to return')
 parser.add_argument('limit', type=int, default=5, help='number of alerts to return')
+
+# insert alert parser
+insert_parser = reqparse.RequestParser()
+insert_parser.add_argument('host_name', type=str, required=True, help='host name')
+insert_parser.add_argument('host_state', type=str, required=True, help='host state')
+insert_parser.add_argument('host_address', type=str, required=True, help='host state')
+insert_parser.add_argument('n_datetime', type=str, required=True, help='host state')
+insert_parser.add_argument('service_name', type=str, required=True, help='host state')
+insert_parser.add_argument('notification_type', type=str, required=True, help='host state')
+insert_parser.add_argument('alert_type', type=str, required=True, help='host state')
+insert_parser.add_argument('output_text', type=str, required=True, help='host state')
+
+
+MESSAGE_TEXT="""
+***** Nagios *****
+Notification Type: {alert_type}
+{host_or_service_line}
+State: {host_state}
+Address: {host_address}
+Info: {output_text}
+Date/Time: {n_datetime}
+"""
 
 
 @api.route('/')
@@ -50,18 +67,24 @@ class NagiosAlerts(Resource):
         cursor.execute(query)
         return {'alerts': list(cursor)}
 
-    @api.expect(NagiosInsertAlertModel, validate=True)
+    @api.expect(insert_parser, validate=True)
     def post(self):
         """
         Allows the addition of new alerts
         :return:
         """
-        alert_data = request.json
+        alert_data = insert_parser.parse_args()
+        alert_data['n_datetime'] = parse(alert_data['n_datetime']) if alert_data['n_datetime'] else datetime.datetime.utcnow()
+        alert_data['host_or_service_line'] = ""
+        if alert_data['alert_type'].upper() == 'HOST':
+            alert_data['host_or_service_line'] = "HOST:{0}".format(alert_data['host_name'])
+
+        message_text = MESSAGE_TEXT.format(**alert_data)
 
         conn = mysql_nagios.get_db()
         cursor = conn.cursor()
-        query = "INSERT INTO `nagios_alerts`(`message_text`, `hostname`, `service_name`, `notification_type`) VALUES (%s, %s, %s, %s)"
-        cursor.execute(query, (alert_data['message_text'], alert_data['hostname'], alert_data['service_name'], alert_data['notification_type']))
+        query = "INSERT INTO `nagios_alerts`( `date_inserted`, `message_text`, `hostname`, `service_name`, `notification_type`) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(query, (alert_data['n_datetime'], message_text, alert_data['host_name'], alert_data['service_name'], alert_data['notification_type']))
         conn.commit()
         app.logger.info("Inserted alert successfully")
 
