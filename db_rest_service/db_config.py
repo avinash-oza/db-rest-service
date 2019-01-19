e25a4e3fa14e20af8301aa98a0771da80f7a063d
Avi Oza <avioza@gmail.com>
2019-01-19 15:28:28 -0500

    allow env vars for settings

--- db_rest_service/db_config.py
+++ db_rest_service/db_config.py
@@ -1,10 +1,14 @@
+import os
 from db_rest_service import app
 from flaskext.mysql import MySQL
 
 mysql_nagios = MySQL()
- # TODO: proper passwords and settings here
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'my-secret-pw'
-app.config['MYSQL_DATABASE_DB'] = 'telegram_bot'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# TODO: proper passwords and settings here
+keys_to_defaults = {'MYSQL_DATABASE_USER': 'root',
+                    'MYSQL_DATABASE_PASSWORD': 'my-secret-pw',
+                    'MYSQL_DATABASE_DB': 'telegram_bot',
+                    'MYSQL_DATABASE_HOST': 'localhost'}
+for key, default in keys_to_defaults.items():
+    app.config[key] = os.environ.get(key, default)
+
 mysql_nagios.init_app(app)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='db-rest-service',
-        version='0.1b1',
+        version='0.1b2',
         packages=['db_rest_service',],
         license='TBD',
         long_description='TBD'
