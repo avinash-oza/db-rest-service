6f6688d50bc84a3b85a2aeb442295ed4d0cc8cec
Avi Oza <avioza@gmail.com>
2019-02-02 19:31:44 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='db-rest-service',
-        version='0.1b8',
+        version='0.1b9',
         packages=find_packages(),
         license='TBD',
         long_description='TBD'
